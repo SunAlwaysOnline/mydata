6cb0434c9818927423afe31cb422daed9377149c
chenqiong <chenqiong@beyondcent.com>
2019-05-21 17:31:38 +0800

    add branch name in controller

--- src/main/java/com/sun/mydata/controller/AccountController.java
+++ src/main/java/com/sun/mydata/controller/AccountController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController=@Controller+@ResponseBody
-//second分支
+//master分支
 @RestController
 @RequestMapping("/account")
 public class AccountController {

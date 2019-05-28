a515b9e52ad7ae995effd99ab97a88b6803e29be
chenqiong <chenqiong@beyondcent.com>
2019-05-28 11:40:16 +0800

    modify comment

--- src/main/java/com/sun/mydata/controller/AccountController.java
+++ src/main/java/com/sun/mydata/controller/AccountController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController=@Controller+@ResponseBody
-//master分支
-//second分支
-//继续进行master分支的开发
+//这是主分支
 @RestController
 @RequestMapping("/account")
 public class AccountController {
